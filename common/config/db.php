5574682ff9e3bb1f8810b02b1d18d8e73e6fb5a2
liujing <1234@qq.com>
2020-06-02 11:01:50 +0800

    修改数据库信息

--- common/config/db.php
+++ common/config/db.php
@@ -1,7 +1,8 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=my',
+//    'dsn' => 'mysql:host=localhost;dbname=my',
+    'dsn' => 'mysql:host=192.168.2.104;port=3306;dbname=my',
     'username' => 'root',
     'password' => '123456',
     'charset' => 'utf8',
